ff0bffb20836af739e6b88cfbe26b1dbd0569de3
zhongzhouTan-coder <1710115119@bjmu.edu.cn>
2023-07-31 22:21:12 +0800

    [2023-7-31][feat] implement command line support for classpath and classname when jvm run

--- jvm/src/main.rs
+++ jvm/src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+#[derive(Parser, Debug, Default)]
+#[clap(
+    author = "Tang xuan",
+    version,
+    about = "A simple java virtual machine cli."
+)]
+struct Args {
+    /// Specifies a list of directories, JAR files, and ZIP archives to search for class files.
+    #[clap(long = "class-path")]
+    classpath: std::path::PathBuf,
+
+    /// Sets the fully qualified classname to execute.
+    #[clap(long = "class-name")]
+    classname: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
+    run_jvm(args.classpath, &args.classname)
+}
+
+fn run_jvm(classpath: std::path::PathBuf, classname: &str) {
+    println!("Running JVM with classpath: {:?}", classpath);
+    println!("Executing Java class: {}", classname);
 }
